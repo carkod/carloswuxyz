cbba065c5be2849e321a78284ec427767cd2a476
Carkod <carkodesign@gmail.com>
2016-03-17 03:55:49 +0000

    new checkers theme from repo

--- wp-content/themes/checkers0.7/config.rb
+++
@@ -1,7 +0,0 @@
-http_path = "/"
-css_dir = "/"
-sass_path = "/sass/"
-images_dir = "images"
-javascripts_dir = "js"
-sourcemap = true
-output_style = :compact
--- wp-content/themes/checkers0.7/logo.php
+++
@@ -1,5 +0,0 @@
-<header id="masthead" class="site-header" role="banner">
-    <h1><strong><?php bloginfo('name')?></strong></h1>
-    <h2><?php bloginfo('description')?></h2>
-    <!-- .site-branding -->
-</header><!-- .site-header -->
